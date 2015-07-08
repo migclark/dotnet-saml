74073ea73571a415498ed240c79341fdb4e30493
Miguel Clark <migclark@gmail.com>
2015-07-08 15:54:04 -0700

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,10 +16,11 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Response.Write("<div>Page_Load</div>");
+        
         AccountSettings accountSettings = new AccountSettings();
 
-        OneLogin.Saml.AuthRequest req = new AuthRequest(new AppSettings(), accountSettings);
-        
+        //OneLogin.Saml.AuthRequest req = new AuthRequest(new AppSettings(), accountSettings);
         //Response.Redirect(accountSettings.idp_sso_target_url + "?SAMLRequest=" + Server.UrlEncode(req.GetRequest(AuthRequest.AuthRequestFormat.Base64)));
     }
 }
